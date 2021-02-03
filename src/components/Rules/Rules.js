c1f0099cc266a598e5bfcf9114fc542150ff98a3
tomasaccini <tomasaccini@gmail.com>
2021-02-03 15:17:35 -0300

    mock rules values

--- src/components/Rules/Rules.js
+++ src/components/Rules/Rules.js
@@ -1,8 +1,64 @@
 import React from 'react';
 import RulesContainer from './RulesContainer'
 
-export default function Rules() {
-  return (
-    <RulesContainer />
-  );
+export default class Rules extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onDragEnd = this.onDragEnd.bind(this);
+    this.state = {rules: []};
+  }
+
+  componentDidMount() {
+    this.getCurrentRules();
+  }
+
+  getCurrentRules() {
+    // We should hit an endpoint to get the current rules
+    const rules = this.mockRules();
+    this.setState({
+      rules: rules
+    });
+  }
+  
+  reorder(list, startIndex, endIndex) {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+  
+    result.forEach(function (rule, i) {
+      rule.index = i;
+    });
+
+    return result;
+  }
+
+  onDragEnd(result) {
+		if (!result.destination) {
+			return;
+		}
+
+		if (result.destination.index === result.source.index) {
+			return;
+    }
+		const newRules = this.reorder(
+			this.state.rules,
+			result.source.index,
+			result.destination.index
+		);
+    this.setState({ rules: newRules });
+    console.log("state:", this.state);
+	}
+
+  mockRules() {
+    return [
+      {id: 0, index: 0, contagionRisk: 'Alto', durationValue: 60, durationCmp: '>', m2Value: 30, m2Cmp: '<', spaceValue: 'Cerrado'},
+      {id: 1, index: 1, contagionRisk: 'Bajo', durationValue: 60, durationCmp: '<', m2Value: 50, m2Cmp: '>', spaceValue: 'Abierto'},
+      {id: 2, index: 2, contagionRisk: 'Alto', durationValue: 120, durationCmp: '>'},
+      {id: 3, index: 3, contagionRisk: 'Bajo', m2Value: 100, m2Cmp: '>', spaceValue: 'Abierto'}
+    ];
+  }
+
+  render() {
+    return <RulesContainer rules={this.state.rules} onDragEnd={this.onDragEnd}/>;
+  }
 }
--- src/components/Rules/RulesContainer.js
+++ src/components/Rules/RulesContainer.js
@@ -1,31 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import RuleCard from "./RuleCard";
-
-const initialRules = Array.from({ length: 10 }, (v, k) => k).map(k => {
-	const custom: Rule = {
-		id: k,
-		contagionRisk: `Contagio ${k}`
-	};
-
-	return custom;
-
-});
 
 function Rule({ rule, index }) {
 	return (<RuleCard id={rule.id}
 		contagionRisk={rule.contagionRisk}
 		index={index} />);
 }
-
-const reorder = (list, startIndex, endIndex) => {
-	const result = Array.from(list);
-	const [removed] = result.splice(startIndex, 1);
-	result.splice(endIndex, 0, removed);
-
-	return result;
-};
 
 const RulesList = React.memo(function RulesList({ rules }) {
 	return rules.map((rule, index) => (
@@ -49,31 +31,17 @@ const useStyles = makeStyles((theme) => ({
 	}
 }));
 
-export default function RulesContainer() {
-	const [state, setState] = useState({ rules: initialRules });
+export default function RulesContainer(props) {
+	const [state, setState] = useState({ rules: props.rules || [] });
 	const classes = useStyles();
 
-	function onDragEnd(result) {
-		if (!result.destination) {
-			return;
-		}
-
-		if (result.destination.index === result.source.index) {
-			return;
-		}
-
-		const rules = reorder(
-			state.rules,
-			result.source.index,
-			result.destination.index
-		);
-
-		setState({ rules });
-	}
+	React.useEffect(() => {
+		setState({rules: props.rules});
+	}, [props.rules])
 
 	return (
 		<div className={classes.container}>
-			<DragDropContext onDragEnd={onDragEnd}>
+			<DragDropContext onDragEnd={props.onDragEnd}>
 				<Droppable droppableId="list">
 					{provided => (
 						<div ref={provided.innerRef} {...provided.droppableProps}>

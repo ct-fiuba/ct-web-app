2a0efc77a5b2b15ff67b09dcc86cff190c81532a
tomasaccini <tomasaccini@gmail.com>
2021-02-15 18:34:36 -0300

    nueva landing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import {
-  Link
-} from "react-router-dom";
-export default function Home() {
-  return (
-    <div>
-      <Link to="/nuevoEstablecimiento">
-        <Button>
-          <p>Creacion de QRs</p>
-        </Button>
-      </Link>
-      <Link to="/reglas">
-        <Button>
-          <p>Reglas de contagio</p>
-        </Button>
-      </Link>    
-    </div>
-  );
+import React, {Component} from 'react';
+import covidIcon from '../../images/covid.svg';
+import '../../css/home.css';
+
+class Home extends Component {
+    render(){
+        return(
+            <section className="home-page">
+                <img className="rotate covid" src={covidIcon} alt="covid"/>
+                <h1 className="home-title">Contact Tracing COVID-19</h1>
+                <p className="home-stitle">
+                    Bienvenido al sistema de contact tracing para encontrar contagios de COVID-19 manteniendo la privacidad de los usuarios.
+                </p>
+                <a id="LandingIngresarButton" href="/nuevoEstablecimiento">Registrar nuevo establecimiento</a> (acceso público)
+                <a id="LandingIngresarButton" href="/reglas">Administrar las reglas de contagio</a> (acceso restringido a administradores)
+            </section>
+        )
+    }
 }
+
+export default Home;

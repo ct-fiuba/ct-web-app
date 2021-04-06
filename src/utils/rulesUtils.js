a3aa04196b3032530beb515bec85e090209d6e10
tomasaccini <tomasaccini@gmail.com>
2021-04-06 15:03:12 -0300

    force order based on risks (Alto, Medio, Bajo)

--- src/utils/rulesUtils.js
+++ src/utils/rulesUtils.js
@@ -1,6 +1,17 @@
-export function updateIndexesFromAddition(new_rules) {
+export function calculateNewRuleIndex(new_rules, contagionRisk) {
+	for (var i = 0; i < new_rules.length; i++) {
+		if (new_rules[i].contagionRisk === contagionRisk){
+			return i;
+		}
+	}
+	return new_rules.length;
+}
+
+export function updateIndexesFromAddition(new_rules, indexAdded) {
 	new_rules.forEach(rule => {
-		rule['index'] = rule['index'] + 1;
+		if (rule['index'] >= indexAdded) {
+			rule['index'] = rule['index'] + 1;
+		}
 	});
 	return new_rules;
 }
@@ -24,6 +35,29 @@ export function reorder(list, startIndex, endIndex) {
 	});
 
 	return result;
+}
+
+export function forceHighMidLowOrder(rules) {
+	const q_per_risk = {'Alto': 0, 'Medio': 0, 'Bajo': 0};
+	rules.forEach(rule => {
+		q_per_risk[rule.contagionRisk] += 1;
+	});
+	const current_index_per_risk = {'Alto': 0, 'Medio': q_per_risk['Alto'], 'Bajo': q_per_risk['Alto'] + q_per_risk['Medio']};
+	rules.forEach(rule => {
+		if (rule['contagionRisk'] === 'Alto') {
+			rule['index'] = current_index_per_risk['Alto'];
+			current_index_per_risk['Alto'] += 1;
+		}
+		if (rule['contagionRisk'] === 'Medio') {
+			rule['index'] = current_index_per_risk['Medio'];
+			current_index_per_risk['Medio'] += 1;
+		}
+		if (rule['contagionRisk'] === 'Bajo') {
+			rule['index'] = current_index_per_risk['Bajo'];
+			current_index_per_risk['Bajo'] += 1;
+		}
+	});
+	return rules.sort((r1, r2) => {return r1.index < r2.index ? -1 : 1});
 }
 
 export function areRulesEqual(new_rules, saved_rules) {

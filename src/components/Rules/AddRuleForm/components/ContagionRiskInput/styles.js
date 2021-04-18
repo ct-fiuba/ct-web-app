19c4e9e04d29f1c7af107b98113bc29c370c66cb
tomasaccini <tomasaccini@gmail.com>
2021-04-18 20:47:56 -0300

    remove unused style classes, adapt the existing ones to make everything fit

--- src/components/Rules/AddRuleForm/components/ContagionRiskInput/styles.js
+++ src/components/Rules/AddRuleForm/components/ContagionRiskInput/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   formControlContagionRisk: {
-    margin: theme.spacing(4),
+    margin: theme.spacing(2),
     width: '400px',
   },
   labelContagionRisk: {

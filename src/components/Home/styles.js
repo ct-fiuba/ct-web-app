d41a38e450c28e74aa77bef3bb09a851cc31088a
tomasaccini <tomasaccini@gmail.com>
2021-05-25 23:51:04 -0300

    typo

--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -10,13 +10,6 @@ const useStyles = makeStyles((theme) => ({
 		fontSize: '15px',
 		textTransform: 'uppercase',
 	},
-	form: {
-		width: '100%', // Fix IE 11 issue.
-		marginTop: theme.spacing(1),
-	},
-	submit: {
-		margin: theme.spacing(3, 0, 2),
-	},
 }));
 
 export default useStyles;

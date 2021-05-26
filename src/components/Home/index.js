965a7f45f3b9ee50dce01f727214815ebf015c47
tomasaccini <tomasaccini@gmail.com>
2021-05-25 23:47:01 -0300

    modify UI for Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,26 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import covidIcon from '../../images/covid.svg';
 import '../../css/home.css';
 import { Button } from '@material-ui/core';
+import useStyles from './styles';
 
-class Home extends Component {
-    render(){
-        return(
-            <section className="home-page">
-                <img className="rotate covid" src={covidIcon} alt="covid"/>
-                <h1 className="home-title">Control de Pandemias</h1>
-                <p className="home-stitle">
-                    Bienvenido al sistema de contact tracing para encontrar contagios de COVID-19 manteniendo la privacidad de los usuarios.
+export default function Home() {
+	const classes = useStyles();
+
+	return (
+		<section className="home-page">
+			<img className="rotate covid" src={covidIcon} alt="covid" />
+			<h1 className="home-title">Control de Pandemias</h1>
+			<p className="home-stitle">
+				Bienvenido al sistema de contact tracing para encontrar contagios de COVID-19 manteniendo la privacidad de los usuarios.
                 </p>
-                <Button href="/nuevoEstablecimiento" color="primary">
-                    Registrar nuevo establecimiento
+			<Button className={classes.ownersButton} href="/nuevoEstablecimiento" variant="contained" color="primary">
+				Ingresar como dueño de establecimiento
+			</Button>
+			<Button className={classes.adminsButton} href="/reglas" color="default">
+				Ingresar como administrador
                 </Button>
-                <Button href="/reglas" color="primary">
-                    Administrar las reglas de contagio (administradores)
-                </Button>
-            </section>
-        )
-    }
+		</section>
+	)
 }
-
-export default Home;

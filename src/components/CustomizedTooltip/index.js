c0a04c5f81b145f778ad67fa4c044bf6622eff98
Oli <olifer97@gmail.com>
2022-07-03 12:47:54 -0300

    clean

--- src/components/CustomizedTooltip/index.js
+++ src/components/CustomizedTooltip/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-//import { MEMES } from '../chartsUtils';
 import './styles.css';
 
 class CustomizedTooltip extends Component {
